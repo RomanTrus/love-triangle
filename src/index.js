fbbfe16bdbdc02032d8b7f0bef56b8bb99f243fe
Raman Trus <radadendronroma@gmail.com>
2018-02-19 22:45:28 +0300

    implement 100/100

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let counter = 0;
+    let i;
+for (i=0;i<preferences.length;i++){
+	//console.log(i+'--'+preferences[i]+'---'+preferences[preferences[i]-1]+'--'+preferences[preferences[preferences[i]-1]-1])
+	if(preferences[preferences[preferences[i]-1]-1]===i+1&&preferences[i]!=preferences[preferences[i]-1]){counter++;
+		preferences[preferences[preferences[i]-1]-1]=undefined;
+		preferences[preferences[i]-1]=undefined;
+		preferences[i]=undefined;
+		}
+}
+	return counter;
 };
